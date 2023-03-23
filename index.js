87cc42d75757133a18bebefde845c5501dfbe59e
gokhalevedant06 <gokhalevedant06@gmail.com>
2023-03-24 00:34:36 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+  res.send("Working");
+});
+
+io.on('connection', (socket) => {
+  console.log('User Connected',socket);
+});
+
+server.listen(process.env.PORT || 3000, () => {
+  console.log(`Listening on PORT : ${process.env.PORT || 3000}`);
+});
